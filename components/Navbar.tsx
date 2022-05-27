496896dbe7d0d697743536469a883f90431cd506
Keningar <acolyte.music1@gmail.com>
2022-05-27 08:17:21 -0500

    second errors fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
@@ -20,16 +21,18 @@ export default function Navbar() {
         >
           <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
             <div className="flex items-center justify-between w-full md:w-auto">
-              <a href="/">
-                <span className="sr-only">Workflow</span>
-                <Image
-                  alt="Workflow"
-                  width={40}
-                  height={40}
-                  layout="fixed"
-                  src="/mararungye.svg"
-                />
-              </a>
+              <Link href="/">
+                <a>
+                  <span className="sr-only">Workflow</span>
+                  <Image
+                    alt="Workflow"
+                    width={40}
+                    height={40}
+                    layout="fixed"
+                    src="/mararungye.svg"
+                  />
+                </a>
+              </Link>
               <div className="-mr-2 flex items-center md:hidden">
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className="sr-only">Open main menu</span>
@@ -40,13 +43,11 @@ export default function Navbar() {
           </div>
           <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
             {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="font-medium text-gray-500 hover:text-gray-900"
-              >
-                {item.name}
-              </a>
+              <Link key={item.name} href={item.href}>
+                <a className="font-medium text-gray-500 hover:text-gray-900">
+                  {item.name}
+                </a>
+              </Link>
             ))}
           </div>
         </nav>
@@ -83,13 +84,11 @@ export default function Navbar() {
             </div>
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                >
-                  {item.name}
-                </a>
+                <Link key={item.name} href={item.href}>
+                  <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">
+                    {item.name}
+                  </a>
+                </Link>
               ))}
             </div>
           </div>
